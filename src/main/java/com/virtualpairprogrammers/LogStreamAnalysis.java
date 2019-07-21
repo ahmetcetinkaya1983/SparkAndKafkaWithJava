ce303b22575e54fb9060139d088bcd9772d439a9
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-07-21 22:29:01 +0300

    init Spark Streaming

---
+++ src/main/java/com/virtualpairprogrammers/LogStreamAnalysis.java
@@ -0,0 +1,17 @@
+package com.virtualpairprogrammers;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+
+public class LogStreamAnalysis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("hadoop.home.dir", "c:/hadoop");	
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("startingSpark");
+	}
+
+}
