0c49d5edfd5b0b08852730dbb6395f0e8b65a1c4
Ahmet ÇETİNKAYA <ahmet.cetinkaya.1983@gmail.com>
2019-07-22 22:14:39 +0300

    Using KafkaUtils to access a DStream

--- src/main/java/com/virtualpairprogrammers/streaming/ViewingFiguresDStreamVersion.java
+++ src/main/java/com/virtualpairprogrammers/streaming/ViewingFiguresDStreamVersion.java
@@ -1,10 +1,23 @@
 package com.virtualpairprogrammers.streaming;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+
 
 public class ViewingFiguresDStreamVersion {
 	public static void main(String[] args) throws InterruptedException {
@@ -16,5 +29,26 @@ public class ViewingFiguresDStreamVersion {
 		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("startingSpark");
 		
 		JavaStreamingContext sc  = new JavaStreamingContext(conf, Durations.seconds(1));
+		
+		Collection<String> topics = Arrays.asList("viewrecords");
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put("bootstrap.servers", "localhost:9092");
+		params.put("key.deserializer", StringDeserializer.class);
+		params.put("value.deserializer", StringDeserializer.class);
+		params.put("group.id", "spark-group");
+		params.put("auto.offset.rest", "latest");
+		
+		JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(sc, LocationStrategies.PreferConsistent(), 
+									  ConsumerStrategies.Subscribe(topics, params));
+		
+		//work
+		JavaDStream<String> results = stream.map(item -> item.value());
+		results.print();
+		
+		sc.start();
+		sc.awaitTermination();
+		
+		
 	}
 }
